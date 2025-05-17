221d396c06e0769502b0c75b3561a1edd92fc1c3
Trobbiani Bruno <114705681+Brujomix@users.noreply.github.com>
2025-05-16 21:27:23 -0300

    asign palet of colors

--- tailwind.preset.js
+++ tailwind.preset.js
@@ -3,19 +3,35 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#0f172a',
-        secondary: '#64748b',
+        //White
+        white: "#FFFF",
+        //black
+        black: "#OOOO",
+        //Blue
+        darkBlue: "#293241",
+        deepBlue: "#3D5A80",
+        mediumBlue: "#98C1D9",
+        lightBlue: "#E0FBFC",
+        //orange
+        darkOrange: "#CC2C00",
+        deepOrange: "#F25600",
+        mediumOrange: "#FF9913",
+        lightOrange: "#FFC523",
+        //gray
+        darkGray: "#3C3C3C",
+        deepGray: "#7A7A7A",
+        mediumGray: "#BEBEBE",
+        lightGray: "#E4E4E5",
       },
       fontFamily: {
-        sans: ['Inter', 'sans-serif'],
+        sans: ["Inter", "sans-serif"], // texto general, UI
+        mono: ["Fira Code", "monospace"], // Titulos Botones, ect
       },
     },
   },
   plugins: [],
-  content: [
-    './**/*.{js,ts,jsx,tsx}', 
-  ],
-  "exclude": [
+  content: ["./**/*.{js,ts,jsx,tsx}"],
+  exclude: [
     "node_modules",
     "dist",
     "build",
@@ -23,6 +39,6 @@ module.exports = {
     "**/*.test.ts",
     "**/*.test.tsx",
     "**/*.spec.ts",
-    "**/*.spec.tsx"
-  ]
+    "**/*.spec.tsx",
+  ],
 };
