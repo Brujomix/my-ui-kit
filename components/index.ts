db7b19980d070816a9ecc49d6eaa94ac2f39b7da
Trobbiani Bruno <Brujomix.bt@gmail.com>
2025-05-22 18:25:30 -0300

    solutions on exports

---
+++ components/index.ts
@@ -0,0 +1,23 @@
+//buttons
+export * from "./tags/Button";
+//sections
+export * from "./sections/Header";
+export * from "./sections/Aside";
+export * from "./sections/NavBar";
+export * from "./sections/Footer";
+//Layouts
+export * from "./layouts/GeneralLayout";
+//Modals
+export * from "./modals/GlobalModalManager";
+//Toast
+export * from "./toast/Toast_Container";
+export * from "./toast/useToast";
+//Icons
+export * from "./icons"
+//Toogle
+export * from "./toggles/ToogleMode"
+//user
+export * from "./user/CurrentUser"
+//Badges
+export * from "./badge/Badge"
+
