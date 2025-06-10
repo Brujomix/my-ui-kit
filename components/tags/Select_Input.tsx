aa4556994cedfbac44dba7dafbaa4953a1393806
Brujomix <brujomix.bt@gmail.com>
2025-06-10 20:53:58 -0300

    fix selectInput

--- components/tags/Select_Input.tsx
+++ components/tags/Select_Input.tsx
@@ -1,35 +1,34 @@
 type SelectProps = React.InputHTMLAttributes<HTMLSelectElement>
 
-type OptionItem = {
-  k: string;
-  v: string | number;
-};
-
-type SelectInputProps = {
-    data : OptionItem[]
-    label ?: string
-    className ? : string
+type SelectInputProps<T> = {
+  data: T[]
+  label: string
+  labelKey: keyof T
+  valueKey: keyof T
+  className?: string
 } & SelectProps
 
-export function Select_Input({
+export function Select_Input<T extends object>({
   className = "",
   data,
   label,
+  valueKey,
+  labelKey,
   ...props
-}: SelectInputProps) {
+}: SelectInputProps<T>) {
   return (
     <div className="grid gap-2">
-    <label>{label}</label>
-    <select
-      className={`px-4 py-2 rounded border border-deepGray bg-lightGray text-darkGray focus:outline-none focus:ring-2 focus:ring-deepBlue ${className}`}
-      {...props}
-    >
-      {data.map(({ k, v}, index) => (
-        <option key={index} value={v}>
-          {k}
-        </option>
-      ))}
-    </select>
+      <label>{label}</label>
+      <select
+        className={`px-4 py-2 rounded border border-deepGray bg-lightGray text-darkGray focus:outline-none focus:ring-2 focus:ring-deepBlue ${className}`}
+        {...props}
+      >
+        {data.map((obj) => (
+          <option key={String(obj[valueKey])} value={String(obj[valueKey])}>
+            {String(obj[labelKey])}
+          </option>
+        ))}
+      </select>
     </div>
   );
 }
