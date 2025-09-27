9b643aed7a5a2142f8aa1b1df91a6f09cdd4cb58
Trobbiani Bruno <114705681+Brujomix@users.noreply.github.com>
2025-09-27 12:54:24 -0300

    styles on avatar and badge

--- components/badges/badge.tsx
+++ components/badges/badge.tsx
@@ -15,11 +15,11 @@ export function Badge ({ children, Icon, color = Colors.secondary }: BadgeProps)
       className={clsx(
         'text-xs font-medium px-2.5 py-0.5 rounded border inline-flex items-center whitespace-nowrap dark:bg-gray-700/25 w-min',
         {
-          'bg-primary-100/25 text-primary-800 border-primary-400': color === Colors.primary,
-          'bg-gray-100/25 text-gray-800 border-gray-500': color === Colors.secondary,
-          'bg-red-100/25 text-red-800 border-red-400': color === Colors.danger,
-          'bg-green-100/25 text-green-800 border-green-400': color === Colors.success,
-          'bg-yellow-100/25 text-yellow-800 border-yellow-300': color === Colors.warning
+          'bg-primary-100/50 text-primary-800 border-primary-600': color === Colors.primary,
+          'bg-gray-100/50 text-gray-800 border-gray-600': color === Colors.secondary,
+          'bg-red-100/50 text-red-800 border-red-600': color === Colors.danger,
+          'bg-green-100/50 text-green-800 border-green-600': color === Colors.success,
+          'bg-yellow-100/50 text-yellow-800 border-yellow-600': color === Colors.warning
         }
       )}
     >
--- components/user/avatar-circle.tsx
+++ components/user/avatar-circle.tsx
@@ -1,28 +1,29 @@
-
 const filterStringText = (text: string) => {
-  return text.slice(0, 2).toUpperCase();
-};
+  return text.slice(0, 2).toUpperCase()
+}
 
 type AvatarClientProps<T> = {
-  user: T;
-  keyField: keyof T;
-  imageUrl?: keyof T;
-};
+  user: T
+  keyField: keyof T
+  imageUrl?: keyof T
+}
 
-export function AvatarCircle<T>({
+export function AvatarCircle<T> ({
   user,
   keyField,
   imageUrl,
 }: AvatarClientProps<T>) {
   return (
-    <div className="text-3xl">
-        {imageUrl ? (
-          <img src={String(user[imageUrl])} alt="Imagen Perfil usuario" />
-        ) : (
-          <span className="font-bold">
+    <div className='text-3xl rounded-full border p-4 bg-gray-600 border-gray-400'>
+      {imageUrl
+        ? (
+          <img src={String(user[imageUrl])} alt='Imagen Perfil usuario' />
+          )
+        : (
+          <span className='font-bold'>
             {filterStringText(String(user[keyField]))}
           </span>
-        )}
+          )}
     </div>
-  );
+  )
 }
