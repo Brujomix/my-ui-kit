19774288ddfc38b74186f50083ab33fc254d2f42
Trobbiani Bruno <114705681+Brujomix@users.noreply.github.com>
2025-10-14 17:55:47 -0300

    sizes on Submit button

--- components/badges/badge.tsx
+++ components/badges/badge.tsx
@@ -2,19 +2,30 @@ import clsx from 'clsx'
 import { Colors } from '../tools/colors'
 import { FC } from 'react'
 import { IconProps } from '../icons'
+import { Sizes } from '../tools'
 
 export interface BadgeProps {
   children: string | number | Array<string | number>
   color?: keyof typeof Colors
   Icon?: FC<IconProps>
+  size?: keyof typeof Sizes
 }
 
-export function Badge ({ children, Icon, color = Colors.secondary }: BadgeProps) {
+export function Badge ({ children, Icon, color = Colors.secondary, size = Sizes.md }: BadgeProps) {
   return (
 
     <span
       className={clsx(
-        'text-xs font-medium px-2.5 py-0.5 rounded border inline-flex items-center whitespace-nowrap dark:bg-gray-700/25 w-min',
+        'font-medium px-2.5 py-0.5 rounded border inline-flex items-center whitespace-nowrap dark:bg-gray-700/25 w-min',
+        {
+          'text-sm ': size === Sizes.sm,
+          'text-base ': size === Sizes.md,
+          'text-lg': size === Sizes.lg,
+          'text-xl': size === Sizes.xl,
+          'text-2xl': size === Sizes.xl,
+          'text-3xl': size === Sizes.xxl,
+          'text-4xl': size === Sizes.xxxl
+        },
         {
           'bg-primary-200 text-primary-800 border-primary-800': color === Colors.primary,
           'bg-gray-200 text-gray-800 border-gray-800 dark:border-gray-400 dark:text-gray-400': color === Colors.secondary,
