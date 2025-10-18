61b0c15ea28ea55b25345c96bf258a0a0fd7b532
Trobbiani Bruno <114705681+Brujomix@users.noreply.github.com>
2025-10-18 16:20:31 -0300

    fix close sidebar

--- components/sections/Main.tsx
+++ components/sections/Main.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react'
+import { useSidebarContext } from '../../hooks/sidebar/sidebar-context'
 
 type MainProps = {
   children?: ReactNode
@@ -7,8 +8,11 @@ type MainProps = {
 }
 
 export function Main ({ children, headerHeight, footerHeight }: MainProps) {
+  const { isOpen, openCloseSidebar } = useSidebarContext()
+
   return (
     <div
+      onClick={() => isOpen && openCloseSidebar()}
       className='p-2 flex-1 text-black dark:text-gray-100'
       style={{
         marginTop: headerHeight && headerHeight,
