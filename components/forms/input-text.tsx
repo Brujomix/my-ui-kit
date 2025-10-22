4b6cbd2694ba19cffc9b51a8d27e7b36b429b406
Trobbiani Bruno <114705681+Brujomix@users.noreply.github.com>
2025-10-21 22:50:19 -0300

    add type text area

--- components/forms/input-text.tsx
+++ components/forms/input-text.tsx
@@ -20,9 +20,11 @@ interface Props {
   disabled?: boolean
   widthMin?: boolean
   maxHeight?: boolean
+  asTextArea?: boolean
+  rows?: number
 }
 
-export function InputText ({ name, disabled, label, convertEmptyToUndefined = true, button, placeholder, Icon, widthMin, activeTrim, size = Sizes.md, maxHeight = false }: Props) {
+export function InputText ({ name, disabled, label, convertEmptyToUndefined = true, button, placeholder, Icon, widthMin, activeTrim, size = Sizes.md, maxHeight = false, asTextArea = false, rows = 3 }: Props) {
   const { register, formState: { errors } } = useFormContext()
 
   const three = name.split('.')
@@ -42,38 +44,76 @@ export function InputText ({ name, disabled, label, convertEmptyToUndefined = tr
       label={label}
       error={errorMessage}
     >
-      <input
-        disabled={disabled}
-        placeholder={placeholder}
-        className={clsx({
-          'text-base': size === undefined,
-          'text-sm': size === 'sm',
-          'text-md': size === 'md',
-          'text-lg': size === 'lg',
-          'text-xl': size === 'xl',
-          'text-2xl': size === 'xxl',
-          'text-4xl': size === 'xxxl'
-        },
-        'font-medium bg-white dark:bg-gray-800 border rounded-lg p-2.5 focus:outline-none focus:ring-1 ',
-        {
-          'border-gray-200 dark:border-gray-700 focus:ring-gray-300/60': errorMessage == null,
-          'border-red-600/40 dark:border-red-600/50 focus:ring-red-600/30': errorMessage != null,
-          'text-gray-900 dark:text-gray-100': !disabled,
-          'text-gray-900/30 dark:text-gray-100/30': disabled,
-          'pl-10': Icon != null
-        }
-        )}
-        {...register(name, {
-          setValueAs:
-            convertEmptyToUndefined
-              ? (v) => v === ''
-                  ? undefined
-                  : activeTrim
-                    ? v.trim()
-                    : v
-              : undefined
-        })}
-      />
+      {asTextArea
+        ? (
+          <textarea
+            disabled={disabled}
+            placeholder={placeholder}
+            rows={rows}
+            className={clsx({
+              'text-base': size === undefined,
+              'text-sm': size === 'sm',
+              'text-md': size === 'md',
+              'text-lg': size === 'lg',
+              'text-xl': size === 'xl',
+              'text-2xl': size === 'xxl',
+              'text-4xl': size === 'xxxl'
+            },
+            'font-medium h-full bg-white dark:bg-gray-800 border rounded-lg p-2.5 focus:outline-none focus:ring-1 ',
+            {
+              'border-gray-200 dark:border-gray-700 focus:ring-gray-300/60': errorMessage == null,
+              'border-red-600/40 dark:border-red-600/50 focus:ring-red-600/30': errorMessage != null,
+              'text-gray-900 dark:text-gray-100': !disabled,
+              'text-gray-900/30 dark:text-gray-100/30': disabled,
+              'pl-10': Icon != null
+            }
+            )}
+            {...register(name, {
+              setValueAs:
+              convertEmptyToUndefined
+                ? (v) => v === ''
+                    ? undefined
+                    : activeTrim
+                      ? v.trim()
+                      : v
+                : undefined
+            })}
+          />
+          )
+        : (
+          <input
+            disabled={disabled}
+            placeholder={placeholder}
+            className={clsx({
+              'text-base': size === undefined,
+              'text-sm': size === 'sm',
+              'text-md': size === 'md',
+              'text-lg': size === 'lg',
+              'text-xl': size === 'xl',
+              'text-2xl': size === 'xxl',
+              'text-4xl': size === 'xxxl'
+            },
+            'font-medium h-full bg-white dark:bg-gray-800 border rounded-lg p-2.5 focus:outline-none focus:ring-1 ',
+            {
+              'border-gray-200 dark:border-gray-700 focus:ring-gray-300/60': errorMessage == null,
+              'border-red-600/40 dark:border-red-600/50 focus:ring-red-600/30': errorMessage != null,
+              'text-gray-900 dark:text-gray-100': !disabled,
+              'text-gray-900/30 dark:text-gray-100/30': disabled,
+              'pl-10': Icon != null
+            }
+            )}
+            {...register(name, {
+              setValueAs:
+              convertEmptyToUndefined
+                ? (v) => v === ''
+                    ? undefined
+                    : activeTrim
+                      ? v.trim()
+                      : v
+                : undefined
+            })}
+          />
+          )}
       {
         Icon != null && (
           <Icon
